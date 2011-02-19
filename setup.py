09589d431a0672cc332679f815de2151d3d6f47c
Don MacMillen <don@macmillen.net>
2011-02-19 11:37:51 -0800

    Undoing previous change to version number in prep for upload to github

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name             = 'rogues',
-      version          = '9.9.9',
+      version          = '0.2.0',
       test_suite       = 'nose.collector',
       packages         = find_packages(),
       install_requires = ['numpy', 'scipy', 'matplotlib'],
